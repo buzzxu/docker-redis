bb89debb96ebbec1b093a660d867a8e977c6de38
xuxiang <xuxiang@yotur.cn>
2016-11-01 16:13:47 +0800

    23

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,5 @@ COPY redis.conf /data/redis.conf
 COPY start.sh /data/start.sh
 RUN chmod +x /data/start.sh
 RUN chown -R redis:redis /data/*
+EXPOSE 6379/tcp
 ENTRYPOINT ["sh","/data/start.sh"]
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
+#!/bin/sh
 cd /data
 sed -i "s/\$port/$port/g" redis.conf
 sed -i "s/\$databases/$databases/g" redis.conf
-sed -i "s/\$master_port/$master_port/g" redis.conf
 redis-server /data/redis.conf
